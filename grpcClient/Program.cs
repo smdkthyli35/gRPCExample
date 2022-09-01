07a96351af8a35d552646f292187c55a185e6e13
Samed Kütahyalı <smdkthyli@gmail.com>
2022-09-01 10:34:32 +0300

    Added Client-Server connection with Unary rpc.

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,12 +1,23 @@
 ﻿using Grpc.Net.Client;
 using grpcClient;
+using grpcMessageClient;
 
 var channel = GrpcChannel.ForAddress("https://localhost:7133");
-var greeterClient = new Greeter.GreeterClient(channel);
+var messageClient = new Message.MessageClient(channel);
 
-HelloReply result = await greeterClient.SayHelloAsync(new HelloRequest
+MessageResponse response = await messageClient.SendMessageAsync(new MessageRequest
 {
-    Name = "Sa"
+    Message = "Merhaba",
+    Name = "Samed"
 });
 
-System.Console.WriteLine(result.Message);
+System.Console.WriteLine(response.Message);
+
+//var greeterClient = new Greeter.GreeterClient(channel);
+
+//HelloReply result = await greeterClient.SayHelloAsync(new HelloRequest
+//{
+//    Name = "Sa"
+//});
+
+//System.Console.WriteLine(result.Message);
--- grpcServer/Services/GreeterService.cs
+++ grpcServer/Services/GreeterService.cs
@@ -3,7 +3,7 @@ using grpcServer;
 
 namespace grpcServer.Services
 {
-    public class GreeterService : Greeter.GreeterClient
+    public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
         public GreeterService(ILogger<GreeterService> logger)
